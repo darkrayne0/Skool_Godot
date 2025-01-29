ab0ff55c8040756f66e7963618985d0c09f74da1
darkrayne0 <darkrayne0@proton.me>
2025-01-28 22:11:49 -0500

    added more panels,still more to do

--- Scripts/lights.gd
+++ Scripts/lights.gd
@@ -3,7 +3,7 @@ extends Area3D
 var toggle = false #todo - put this variable into the lights and toggle from there it should work for any lights this switch is attched to... maybe
 
 @export var lights: SpotLight3D
-@onready var sound: AudioStreamPlayer = $"../../../../Audio/Lights"
+@export var sound: AudioStreamPlayer
 #temp - not sure what lights I want of where but here is a place holder :)
 #opt - probably make each switch a seperate set of lights as is 2 switches in the same room activate weirdly
 #otherwise could prolly do something like get_node("SpotLight3D path").visable and do some rewriting...
